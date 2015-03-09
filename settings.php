820461a3fcbf9416ef4457c0d0ad1c76f94cf863
neswill1 <nenadkonatar@live.ac.me>
2015-03-09 15:37:22 +0100

    ispravak splesa

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="12101 Greenville Ave,Ste 118 (at Markville) Dallas, TX 75243"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="Dallas";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica="bp"; // AKo ne znate skracenicu ostavite praznu
+$telephone=9729949110;// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
